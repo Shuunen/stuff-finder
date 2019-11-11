dc36a8d47b5b9a3cee45cba7d112ec6a5e185225
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-11-11 02:20:21 +0100

    add form & modal wc

--- src/main.js
+++ src/main.js
@@ -10,71 +10,70 @@ class App extends BaseModel {
     super('app')
     this.log('constructor')
     this.items = []
-  }
-
-  setupListeners () {
-    this.on('settings-set', this.onSettingsSet)
+    this.on('app-form--settings--set', this.onSettingsSave)
+    this.on('app-form--settings--save', this.onSettingsSave)
     this.on('storage-found', this.onStorageFound)
     this.on('search-start', this.onSearchStart)
     this.on('search-retry', this.onSearchRetry)
-  }
-
-  onLoad () {
-    this.emit('do-prompt', ['Stuff Finder', 1000, `Stuff Finder\n${this.coolAscii()}`])
-  }
-
-  afterLoad () {
-    this.emit('settings-action-required', { required: true })
-    this.emit('storage-search', 'api-credentials')
-    setTimeout(() => this.emit('search-start', 'batterie'), 1000)
+    this.on('fade-in', this.fadeIn)
+    this.on('fade-out', el => this.fadeOut(el))
+    this.on('fade-out-destroy', el => this.fadeOut(el, true))
+    setTimeout(() => {
+      this.settingsTriggerEl = document.querySelector('.app-settings--trigger')
+      this.settingsActionRequired(true)
+      this.emit('storage-search', 'api-credentials')
+      this.showTitle()
+    }, 300)
   }
 
   coolAscii () {
     return window.pickOne(['( ＾◡＾)', '♥‿♥', '八(＾□＾*)', '(◡ ‿ ◡ ✿)', '(=^ェ^=)', 'ʕ •ᴥ•ʔ', '(*°∀°)', '\\(-ㅂ-)/', 'ლ(╹◡╹ლ)', 'ლ(o◡oლ)', '＼(＾O＾)／'])
   }
 
-  onSettingsSet (settings) {
-    const { base, key, table, view } = settings
-    if (base && key && table && view) {
-      const credentials = { base, key, table, view }
-      this.tryCredentials(credentials)
-    } else {
-      this.emit('settings-action-required', { required: true })
-    }
+  showTitle () {
+    this.emit('do-prompt', ['Stuff Finder', 1000, `Stuff Finder\n${this.coolAscii()}`])
   }
 
-  async tryCredentials (credentials) {
-    this.log('trying credentials', credentials)
-    const itemsLoaded = await this.loadItems(credentials)
+  async onSettingsSave (settings) {
+    this.log('trying settings', settings)
+    const itemsLoaded = await this.loadItems(settings)
     if (!itemsLoaded) {
-      return this.error('failed to use api credentials', credentials)
+      return this.settingsActionRequired(true, 'failed to use api settings')
     }
-    this.emit('storage-set', { key: 'api-credentials', value: credentials })
+    this.settingsActionRequired(false)
+    this.emit('storage-set', { key: 'api-settings', value: settings })
   }
 
-  async loadItems (credentials) {
+  settingsActionRequired (actionRequired, errorMessage = '') {
+    this.log('set action required to', actionRequired)
+    this.settingsTriggerEl.classList.toggle('action-required', actionRequired)
+    this.emit('app-form--settings--error', errorMessage)
+    if (!actionRequired) {
+      this.emit('app-modal--close')
+    }
+  }
+
+  async loadItems (settings) {
     this.emit('set-loading', true)
-    let response = await this.fetchApi(credentials)
+    let response = await this.fetchApi(settings)
     if (!response || response.error) {
       this.emit('set-loading', false)
-      this.emit('settings-action-required', { required: true, message: 'These credentials are invalid' })
       return false
     }
     let records = response.records
     let offset = response.offset
     while (offset) {
-      response = await this.fetchApi(credentials, offset)
+      response = await this.fetchApi(settings, offset)
       offset = response.offset
       records = records.concat(response.records)
     }
     await this.parseApiRecords(records)
     this.emit('set-loading', false)
-    this.emit('settings-action-required', { required: false, message: '' })
     return true
   }
 
-  async fetchApi (credentials, offset) {
-    let url = `https://api.airtable.com/v0/${credentials.base}/${credentials.table}?api_key=${credentials.key}&view=${credentials.view}`
+  async fetchApi (settings, offset) {
+    let url = `https://api.airtable.com/v0/${settings.base}/${settings.table}?api_key=${settings.key}&view=${settings.view}`
     if (offset) {
       url += '&offset=' + offset
     }
@@ -96,7 +95,19 @@ class App extends BaseModel {
     // https://fusejs.io/
     const options = {
       threshold: 0.45, // 0 is perfect match
-      keys: ['Nom', 'Marque', 'Référence'], // TODO: this is not generic ^^"
+      keys: [{
+        name: 'Nom',
+        weight: 0.5,
+      }, {
+        name: 'Marque',
+        weight: 0.3,
+      }, {
+        name: 'Référence',
+        weight: 0.8,
+      }, {
+        name: 'Catégorie',
+        weight: 0.2,
+      }], // TODO: this is not generic ^^"
     }
     this.fuse = new Fuse(this.items, options)
   }
@@ -113,19 +124,46 @@ class App extends BaseModel {
 
   getSearchResult (data) {
     let name = `${data.Nom} ${data.Marque || ''}`.trim()
-    let details = [(data.Référence || ''), (data.Boite ? `[${data.Boite}${data.Tiroir}]` : '')].join(' ').trim()
+    let details = [(data.Référence || ''), (data.Boite ? `[${data.Boite}${data.Tiroir || ''}]` : '')].join(' ').trim()
     const location = (data.Pièce && data.Pièce !== 'N/A') ? data.Pièce : ''
     if (!location) {
       name += ' ' + details
       details = ''
     }
-    return { name, details, location }
+    return { ...data, name, details, location }
   }
 
   onStorageFound (data) {
     if (data.key === 'api-credentials') {
-      this.emit('settings-set', data.value)
+      this.emit('app-form--settings--set', data.value)
     }
+  }
+
+  async fadeIn (el) {
+    this.log('fadeIn')
+    if (!el.classList.contains('hide')) {
+      this.warn('please add "hide" class before mounting dom element and then call fade-in')
+      return
+    }
+    await this.sleep(10)
+    el.style.opacity = 1
+  }
+
+  async fadeOut (el, destroy = false) {
+    this.log('fadeOut')
+    el.classList.add('hide')
+    await this.sleep(350)
+    el.classList.remove('hide')
+    el.classList.add('hidden')
+    if (!destroy) {
+      return
+    }
+    await this.sleep(350)
+    el.remove()
+  }
+
+  async sleep (ms) {
+    return new Promise(resolve => setTimeout(resolve, (ms || 1000)))
   }
 }
 
