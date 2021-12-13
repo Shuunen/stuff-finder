e68e9a8a676fbb69bcc5adb402fdfe5dcccb2038
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-12-13 03:01:08 +0100

    common data list handling

--- src/components/edit-item.ts
+++ src/components/edit-item.ts
@@ -2,17 +2,15 @@ import { emit, fillTemplate, on, sleep } from 'shuutils'
 
 window.customElements.define('app-edit-item', class extends HTMLElement {
   modal?: HTMLElement
-  template = ''
   edit (item: Item) {
     console.log('edit', item)
-    const locations = item.locations.map(l => `<option value=${l} ${l.toLowerCase() === item.location.toLowerCase() ? 'selected' : ''}>${l}</option>`).join('')
-    const boxes = item.boxes.map(b => `<option value="${b}" ${b.toLowerCase() === item.box.toLowerCase() ? 'selected' : ''}>${b}</option>`).join('')
-    const statuses = item.statuses.map(s => `<option value="${s}" ${s.toLowerCase() === item.status.toLowerCase() ? 'selected' : ''}>${s}</option>`).join('')
-    const drawers = ['', 1, 2, 3, 4, 5, 6, 7].map(d => `<option value="${d}" ${d.toString().toLowerCase() === item.drawer.toLowerCase() ? 'selected' : ''}>${d}</option>`).join('')
+    const template = document.querySelector('template#edit-item')
+    if (!template) return console.error('no edit-item template found')
     const photo = item.photo === undefined ? '' : item.photo[0].url
-    const data = { ...item, photo, statuses, drawers, locations, boxes }
+    const data = { id: item.id, photo }
     if (!this.modal) return console.error('no modal')
-    this.modal.innerHTML = fillTemplate(this.template, data)
+    this.modal.innerHTML = fillTemplate(template.innerHTML, data)
+    emit('app-form--edit-item--set', item)
     const printed = this.modal.querySelector('input[name="ref-printed"]') as HTMLInputElement
     printed.checked = data['ref-printed']
     emit('app-modal--edit-item--open')
@@ -22,9 +20,6 @@ window.customElements.define('app-edit-item', class extends HTMLElement {
     const modal = document.querySelector<HTMLElement>('.app-modal--edit-item .content')
     if (!modal) return console.error('no modal')
     this.modal = modal
-    const template = document.querySelector('template#edit-item')
-    if (!template) return console.error('no template')
-    this.template = template.innerHTML
     on('edit-item', (item: Item) => this.edit(item))
   }
 })
