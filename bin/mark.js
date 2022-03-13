fd80e94b70a6fe309281e84b84bb86a55d2c4b83
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-03-13 16:46:26 +0100

    upgrade lint

--- bin/mark.js
+++ bin/mark.js
@@ -4,7 +4,7 @@ const pkg = require('../package.json')
 const { execSync } = require('child_process')
 
 const location = 'public/index.html'
-const content = fs.readFileSync(location, 'utf-8')
+const content = fs.readFileSync(location, 'utf8')
 const lastCommit = execSync('git rev-parse --short HEAD').toString().trim()
 const now = (new Date().toISOString()).split('.')[0].replace('T', ' ')
 
