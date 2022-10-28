e3d5bafc945319f8cc9999d04c1f1287dddb36ad
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-10-29 00:58:53 +0200

    from id to input data

--- src/services/print.service.ts
+++ src/services/print.service.ts
@@ -1,25 +1,25 @@
-export const itemToPrintText = (item: Item): string => [item.name, item.brand, item.details].join(' ').replace(/\s{2,}/g, ' ').trim()
+export const inputToPrintText = (input: PrintOneInputData): string => [input.name, input.brand, input.details].join(' ').replace(/\s{2,}/g, ' ').trim()
 
-export const itemToPrintCode = (item: Item): string => item.reference.trim() || item.barcode.trim()
+export const inputToPrintCode = (input: PrintOneInputData): string => input.reference.trim() || input.barcode.trim()
 
-export const itemToPrintQrCodeVisual = (item: Item): string => {
-  const code = itemToPrintCode(item)
+export const inputToPrintQrCodeVisual = (input: PrintOneInputData): string => {
+  const code = inputToPrintCode(input)
   if (!code) return '<div class="bg-red-700 text-white">No reference or barcode</div>'
   return `<qr-code data="${code}" margin=0 modulesize=3></qr-code>`
 }
 
-export const itemToPrintLocation = (item: Item): string => {
-  if (!item.box) return item.location ?? ''
+export const itemToPrintLocation = (input: PrintOneInputData): string => {
+  if (!input.box) return input.location ?? ''
   /* c8 ignore next */
-  const box = (item.box.trim()[0] ?? '').toUpperCase()
-  const drawer = (item.drawer?.[0] || '').toUpperCase()
+  const box = (input.box.trim()[0] ?? '').toUpperCase()
+  const drawer = (input.drawer?.[0] || '').toUpperCase()
   return `${box}${drawer}`.trim()
 }
 
-export const itemToPrintData = (item: Item): PrintData => ({
-  location: itemToPrintLocation(item),
-  qrCodeValue: itemToPrintCode(item),
-  qrCodeVisual: itemToPrintQrCodeVisual(item),
-  text: itemToPrintText(item),
+export const inputToPrintData = (input: PrintOneInputData): PrintData => ({
+  location: itemToPrintLocation(input),
+  qrCodeValue: inputToPrintCode(input),
+  qrCodeVisual: inputToPrintQrCodeVisual(input),
+  text: inputToPrintText(input),
 })
 
--- tests/print.test.ts
+++ tests/print.test.ts
@@ -1,5 +1,5 @@
 import { check, checksRun } from 'shuutils'
-import { itemToPrintData, itemToPrintLocation, itemToPrintQrCodeVisual, itemToPrintText } from '../src/services/print.service'
+import { inputToPrintData, inputToPrintQrCodeVisual, inputToPrintText, itemToPrintLocation } from '../src/services/print.service'
 
 const itemA = {
   name: 'name',
@@ -10,27 +10,27 @@ const itemA = {
   box: ' box ',
   drawer: '2',
   location: 'location',
-} as Item
+} as PrintOneInputData
 
-check('itemToPrintText A', itemToPrintText(itemA), 'name brand details')
-check('itemToPrintText B', itemToPrintText({ ...itemA, name: '  ' }), 'brand details')
-check('itemToPrintText C', itemToPrintText({ ...itemA, brand: '  ' }), 'name details')
+check('itemToPrintText A', inputToPrintText(itemA), 'name brand details')
+check('itemToPrintText B', inputToPrintText({ ...itemA, name: '  ' }), 'brand details')
+check('itemToPrintText C', inputToPrintText({ ...itemA, brand: '  ' }), 'name details')
 
-check('itemToPrintBarcode A', itemToPrintQrCodeVisual(itemA), '<qr-code data="reference" margin=0 modulesize=3></qr-code>')
-check('itemToPrintBarcode B', itemToPrintQrCodeVisual({ ...itemA, reference: '  ', barcode: '' }), '<div class="bg-red-700 text-white">No reference or barcode</div>')
-check('itemToPrintBarcode C', itemToPrintQrCodeVisual({ ...itemA, barcode: '  ', reference: '' }), '<div class="bg-red-700 text-white">No reference or barcode</div>')
-check('itemToPrintBarcode D', itemToPrintQrCodeVisual({ ...itemA, reference: '' }), '<qr-code data="barcode" margin=0 modulesize=3></qr-code>')
-check('itemToPrintBarcode E', itemToPrintQrCodeVisual({ ...itemA, barcode: '' }), '<qr-code data="reference" margin=0 modulesize=3></qr-code>')
+check('itemToPrintBarcode A', inputToPrintQrCodeVisual(itemA), '<qr-code data="reference" margin=0 modulesize=3></qr-code>')
+check('itemToPrintBarcode B', inputToPrintQrCodeVisual({ ...itemA, reference: '  ', barcode: '' }), '<div class="bg-red-700 text-white">No reference or barcode</div>')
+check('itemToPrintBarcode C', inputToPrintQrCodeVisual({ ...itemA, barcode: '  ', reference: '' }), '<div class="bg-red-700 text-white">No reference or barcode</div>')
+check('itemToPrintBarcode D', inputToPrintQrCodeVisual({ ...itemA, reference: '' }), '<qr-code data="barcode" margin=0 modulesize=3></qr-code>')
+check('itemToPrintBarcode E', inputToPrintQrCodeVisual({ ...itemA, barcode: '' }), '<qr-code data="reference" margin=0 modulesize=3></qr-code>')
 
-check('itemToPrintLocation A', itemToPrintLocation({ box: 'A', drawer: '4' } as Item), 'A4')
-check('itemToPrintLocation B', itemToPrintLocation({ box: 'A', drawer: '' } as Item), 'A')
-check('itemToPrintLocation C', itemToPrintLocation({ box: '', drawer: '4' } as Item), '')
-check('itemToPrintLocation D', itemToPrintLocation({ box: '', drawer: '' } as Item), '')
-check('itemToPrintLocation E', itemToPrintLocation({ box: 'A', drawer: '4', location: 'Salon' } as Item), 'A4')
-check('itemToPrintLocation F', itemToPrintLocation({ box: 'A', drawer: '', location: 'Salon' } as Item), 'A')
-check('itemToPrintLocation G', itemToPrintLocation({ box: '', drawer: '4', location: 'Salon' } as Item), 'Salon')
+check('itemToPrintLocation A', itemToPrintLocation({ box: 'A', drawer: '4' } as PrintOneInputData), 'A4')
+check('itemToPrintLocation B', itemToPrintLocation({ box: 'A', drawer: '' } as PrintOneInputData), 'A')
+check('itemToPrintLocation C', itemToPrintLocation({ box: '', drawer: '4' } as PrintOneInputData), '')
+check('itemToPrintLocation D', itemToPrintLocation({ box: '', drawer: '' } as PrintOneInputData), '')
+check('itemToPrintLocation E', itemToPrintLocation({ box: 'A', drawer: '4', location: 'Salon' } as PrintOneInputData), 'A4')
+check('itemToPrintLocation F', itemToPrintLocation({ box: 'A', drawer: '', location: 'Salon' } as PrintOneInputData), 'A')
+check('itemToPrintLocation G', itemToPrintLocation({ box: '', drawer: '4', location: 'Salon' } as PrintOneInputData), 'Salon')
 check('itemToPrintLocation H', itemToPrintLocation(itemA), 'B2')
 
-check('itemToPrintData A', itemToPrintData(itemA), { text: 'name brand details', qrCodeVisual: '<qr-code data="reference" margin=0 modulesize=3></qr-code>', qrCodeValue: 'reference', location: 'B2' })
+check('itemToPrintData A', inputToPrintData(itemA), { text: 'name brand details', qrCodeVisual: '<qr-code data="reference" margin=0 modulesize=3></qr-code>', qrCodeValue: 'reference', location: 'B2' })
 
 checksRun()
