9ffc141f68379e198790c75a55fd9dc43f09e8e3
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-10-28 23:40:18 +0200

    enhance print data making

--- src/services/print.service.ts
+++ src/services/print.service.ts
@@ -1,18 +1,25 @@
-const itemToText = (item: Item): string => [item.name, item.brand, item.details].join(' ').trim()
+export const itemToPrintText = (item: Item): string => [item.name, item.brand, item.details].join(' ').replace(/\s{2,}/g, ' ').trim()
 
-const itemToBarcode = (_item: Item): string => '||||  ||  ||||'
+export const itemToPrintCode = (item: Item): string => item.reference.trim() || item.barcode.trim()
 
-const itemToLocation = (item: Item): string => {
-  if (!item.box) return item.location
-  return `${item.box[0]}${item.drawer?.[0] || ''} ( ${item.location} )`
+export const itemToPrintQrCodeVisual = (item: Item): string => {
+  const code = itemToPrintCode(item)
+  if (!code) return '<div class="bg-red-700 text-white">No reference or barcode</div>'
+  return `<qr-code data="${code}" margin=0 modulesize=3></qr-code>`
 }
 
-const getData = (item: Item): PrintData => ({
-  text: itemToText(item),
-  barcode: itemToBarcode(item),
-  location: itemToLocation(item),
+export const itemToPrintLocation = (item: Item): string => {
+  if (!item.box) return item.location ?? ''
+  /* c8 ignore next */
+  const box = (item.box.trim()[0] ?? '').toUpperCase()
+  const drawer = (item.drawer?.[0] || '').toUpperCase()
+  return `${box}${drawer}`.trim()
+}
+
+export const itemToPrintData = (item: Item): PrintData => ({
+  location: itemToPrintLocation(item),
+  qrCodeValue: itemToPrintCode(item),
+  qrCodeVisual: itemToPrintQrCodeVisual(item),
+  text: itemToPrintText(item),
 })
 
-export const printService = {
-  getData,
-}
