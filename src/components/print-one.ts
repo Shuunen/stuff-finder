ff1386276b0d2abe84c7b436d4a60dc3afad2518
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-10-29 15:15:30 +0200

    adjust qr code size regarding print size

--- src/components/print-one.ts
+++ src/components/print-one.ts
@@ -7,13 +7,28 @@ window.customElements.define('app-print-one', class extends HTMLElement {
   previewElement = div('')
   size: PrintSize | undefined
 
-  preview (input?: PrintOneInputData): void {
+  async preview (input?: PrintOneInputData): Promise<void> {
     logger.log('preview in', this.size)
     if (!this.data) this.data = input === undefined ? undefined : inputToPrintData(input)
     if (!this.data) return logger.showError('no data or input to print')
     const template = find.one(`template#print-one--${this.size}`)
     this.previewElement.innerHTML = fillTemplate(template.innerHTML + '<div class="font-mono mt-4">QR Code value : {{ qrCodeValue }}</div>', { ...this.data })
-    sleep(300).then(() => this.doPrintOne())
+    await this.adjustQrCode()
+    this.doPrintOne()
+  }
+  async adjustQrCode (): Promise<void> {
+    // sometimes some qr code are too big and need to be resized to fit the barcode
+    const preview = find.one('.app-barcode', this.previewElement)
+    const maxHeight = preview.scrollHeight - 5
+    await sleep(200)
+    const wc = find.one<HTMLElement>('qr-code', this.previewElement)
+    // reducing the module size do the trick & reduce their display size
+    if (!wc.shadowRoot) return logger.showError('no shadowRoot for qr-code custom element', wc)
+    if (!wc.shadowRoot.firstElementChild) return logger.showError('no firstElementChild for qr-code custom element shadowRoot', wc.shadowRoot)
+    const height = wc.shadowRoot.firstElementChild.scrollHeight
+    if (height <= maxHeight) return logger.log(`qr code size is ok (${height}px <= ${maxHeight}px)`)
+    logger.log(`qr code size has been reduced, it was too big (${height}px > ${maxHeight}px)`)
+    wc.setAttribute('modulesize', '2')
   }
   onFormChange (form: PrintOneFormData): void {
     logger.log('print one form change', form)
