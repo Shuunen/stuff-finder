99913aefab1fd228378bc9887f58a5c9e9163f7d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-10-29 10:52:33 +0200

    add classes to improve read/select/find

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -6,7 +6,7 @@ window.customElements.define('app-modal', class extends HTMLElement {
   backdrop = div('app-backdrop app-hide pointer-events-none fixed top-0 left-0 z-20 flex h-full w-full flex-col items-center justify-center bg-black/50 align-middle')
   modal = div('app-modal')
   createModal (id: string): HTMLDivElement {
-    const modal = div(`${id} relative z-50 m-4 flex w-full flex-col overflow-hidden rounded bg-white p-4 shadow-md md:w-auto ${this.className}`, this.innerHTML)
+    const modal = div(`app-modal ${id} relative z-50 m-4 flex w-full flex-col overflow-hidden rounded bg-white p-4 shadow-md md:w-auto ${this.className}`, this.innerHTML)
     const close = link(tw('app-close absolute !border-b-0 top-2 right-5 font-mono text-4xl opacity-50 md:text-2xl'), 'x', '#')
     modal.append(close)
     if (this.dataset['title']) modal.prepend(h2('app-header mx-20 mb-4 text-center text-2xl leading-7 text-purple-700', this.dataset['title']))
@@ -17,6 +17,7 @@ window.customElements.define('app-modal', class extends HTMLElement {
     if (active === this.active) return
     this.active = active
     document.body.classList.toggle('overflow-hidden', active)
+    this.modal.classList.toggle('visible', active)
     this.modal.classList.toggle('hidden', !active)
     this.backdrop.classList.toggle('pointer-events-none', !active)
     if (active) fadeIn(this.backdrop)
