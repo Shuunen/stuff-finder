26a67107220340156343856c93b288758053bf0a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-11-08 22:01:41 +0100

    add getAsin util

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -4,3 +4,13 @@ export const urlToUuid = (url: string): string => {
   // out: wrapapicomusejojodeyesjson002code3760052142741
   return url.replace(/(https?|\W|wrapAPIKey.+)/g, '')
 }
+
+export const getAsin = (url: string): string | undefined => {
+  // in: https://www.amazon.fr/dp/B07V7GZQJ2 or B07V7GZQJ2
+  // out: B07V7GZQJ2
+  const split = url.split('/dp/')
+  const check = split[1] ?? url
+  const regex = /(B[\dA-Z]{9}|\d{9}(X|\d))/
+  const match = regex.exec(check) ?? []
+  return match[1]
+}
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,8 +1,13 @@
 import { check, checksRun } from 'shuutils'
-import { urlToUuid } from '../src/utils/url'
+import { getAsin, urlToUuid } from '../src/utils/url'
 
 check('urlToUuid A', urlToUuid('https://wrapapi.com/use/jojo/deyes/json/0.0.2?code=3760052142741&wrapAPIKey=xyz'), 'wrapapicomusejojodeyesjson002code3760052142741')
 check('urlToUuid B', urlToUuid('https://'), '')
 check('urlToUuid C', urlToUuid('https://wrapapi.com/use/jojo/deyes/json/0.0.2?code=3760052142741'), 'wrapapicomusejojodeyesjson002code3760052142741')
+
+check('getAsin A', getAsin('https://www.amazon.fr/dp/B07V7GZQJ2'), 'B07V7GZQJ2')
+check('getAsin B', getAsin('https://www.amazon.fr/dp/B077QN5GZC?pd_rd_w=uUbTg&content-id=amzn1.sym'), 'B077QN5GZC')
+check('getAsin C', getAsin('B077QN5GZC'), 'B077QN5GZC')
+check('getAsin D', getAsin('im-no-t-asin'))
 
 checksRun()
