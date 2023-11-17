2e4ca0c9e4d1cba8618233801ff81a3dadde7fbb
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-11-17 19:22:19 +0100

    add state with persistence

---
+++ src/utils/state.utils.ts
@@ -0,0 +1,10 @@
+import { createState } from 'shuutils'
+import { emptyCredentials } from '../constants'
+import type { Item } from '../types'
+import { storage } from './storage.utils'
+
+export const { state, watchState } = createState({
+  credentials: emptyCredentials,
+  items: [] as Item[], // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  theme: typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+}, storage)
