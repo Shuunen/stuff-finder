bff1f6c0cf6ed85eb76e454732176f880ea9faf4
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-12-30 23:09:42 +0100

    proper page search with fuse <3

--- src/pages/page-search.tsx
+++ src/pages/page-search.tsx
@@ -1,18 +1,38 @@
+import { useSignalEffect } from '@preact/signals'
+import Fuse from 'fuse.js'
+import { useState } from 'preact/hooks'
+import { sanitize } from 'shuutils'
 import { AppItemList } from '../components/app-item-list'
-import { fakeItem } from '../utils/item.utils'
+import { fuseOptions } from '../constants'
+import { defaultItems } from '../types/item.types'
 import { logger } from '../utils/logger.utils'
+import { state } from '../utils/state.utils'
 
-const items = [
-  fakeItem('Sécateur de ouf'),
-  fakeItem('Pelle en acier trempé'),
-  fakeItem('Brouette de jardin'),
-]
+function search (input: string) {
+  const fuse = new Fuse(state.items, fuseOptions)
+  const result = state.items.find(item => item.reference === input || item.barcode === input)
+  const results = result ? [result] : fuse.search(sanitize(input)).map(item => item.item)
+  let header = 'No results found'
+  if (results.length > 0) header = results.length === 1 ? 'One result found' : `${results.length} results found`
+  header += ` for “${input}”`
+  return { header, results }
+}
 
 export function PageSearch ({ input = '', ...properties }: { readonly input?: string; readonly [key: string]: unknown }) {
-  logger.debug('PageSearch', { input, properties })
+  logger.debug('PageSearch rendering', { input, properties })
+  const [items, setItems] = useState(defaultItems)
+  const [title, setTitle] = useState('Searching...')
+  useSignalEffect(() => {
+    logger.debug('PageSearch is mounted')
+    const { header, results } = search(input)
+    setItems(results)
+    setTitle(header)
+  })
+
   return (
-    <div className="flex flex-col gap-6" data-page="search">
-      <h2>Found {items.length} search results for &quot;{input}&quot; :</h2>
+    <div className="flex max-h-full grow flex-col justify-center gap-6" data-page="search">
+      <h1>Search</h1>
+      <h2>{title}</h2>
       <AppItemList items={items} />
     </div>
   )
