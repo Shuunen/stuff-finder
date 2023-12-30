ff06dfb1550f4fe5c7785a9e818802634ec73e7f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-12-30 12:22:26 +0100

    no need to use box

--- src/pages/app-settings.tsx
+++ src/pages/app-settings.tsx
@@ -1,4 +1,3 @@
-import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import { useState } from 'preact/hooks'
@@ -45,8 +44,7 @@ export function AppSettings ({ ...properties }: { readonly [key: string]: unknow
   return (
     <div className="flex flex-col" data-page="settings">
       <h1>Settings</h1>
-      {/* @ts-expect-error typings issue */ /* eslint-disable-next-line react/forbid-component-props */}
-      <Box autoComplete="off" className="mt-8 grid gap-4" component="form" noValidate onSubmit={onSubmit} spellCheck="false">
+      <form autoComplete="off" className="mt-8 grid gap-4" noValidate onSubmit={onSubmit} spellCheck={false}>
         <TextField error={isBaseValid} id="base" label="Airtable base" onChange={event => setBase(event.target.value)} required value={base} variant="standard" />
         <TextField id="token" label="Airtable token" onChange={event => setToken(event.target.value)} required value={token} variant="standard" />
         <TextField id="table" label="Airtable table" onChange={event => setTable(event.target.value)} required value={table} variant="standard" />
@@ -56,7 +54,7 @@ export function AppSettings ({ ...properties }: { readonly [key: string]: unknow
         <div className="flex justify-between">
           <Button type="submit" variant="contained">Save</Button>
         </div>
-      </Box>
+      </form>
     </div>
   )
 }
