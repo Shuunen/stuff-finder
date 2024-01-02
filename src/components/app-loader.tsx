f9c3e01891d5555dc6ac2a630a52048c37c24025
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-01-02 13:32:33 +0100

    fix loader z-index

--- src/components/app-loader.tsx
+++ src/components/app-loader.tsx
@@ -4,7 +4,7 @@ import Fade from '@mui/material/Fade'
 export function AppLoader ({ isLoading }: { readonly isLoading: boolean }) {
   return (
     <Fade in={isLoading}>
-      <div className="fixed left-0 top-0 flex h-full w-full flex-col bg-white/50" data-component="loader">
+      <div className="fixed left-0 top-0 z-50 flex h-full w-full flex-col bg-white/50" data-component="loader">
         <CircularProgress size={50} />
       </div>
     </Fade>
