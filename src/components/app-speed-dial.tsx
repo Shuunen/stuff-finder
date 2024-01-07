16da15b45489ffd8c4116d0ec0abb3f6c2e28051
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-01-07 12:27:43 +0100

    use curry to enhance callback readability in templates

--- src/components/app-speed-dial.tsx
+++ src/components/app-speed-dial.tsx
@@ -10,6 +10,7 @@ import SpeedDialAction from '@mui/material/SpeedDialAction'
 import Zoom from '@mui/material/Zoom'
 import { route } from 'preact-router'
 import { useState } from 'preact/hooks'
+import { voidFunction } from '../constants'
 import { scout } from '../utils/browser.utils'
 import { logger } from '../utils/logger.utils'
 import { state, watchState } from '../utils/state.utils'
@@ -26,18 +27,19 @@ export function AppSpeedDial () {
 
   const [isVisible, setIsVisible] = useState(false)
   const [isOpen, setOpen] = useState(false)
-  function toggleOpen (reason = 'unknown') { logger.debug('toggle cause', reason); setOpen(!isOpen) }
+  function toggleOpen (reason = 'unknown') { logger.debug('toggle cause', reason); setOpen(!isOpen); return voidFunction }
+  function onMouse (status: 'enter' | 'leave') { if (!scout.isMobile) { logger.debug('open cause mouse', status); setOpen(status === 'enter') }; return voidFunction }
   watchState('status', () => setIsVisible(state.status === 'ready'))
 
   return (
     <>
       <Fade in={isOpen}>
-        <div className="absolute bottom-0 right-0 z-10 h-screen w-screen bg-black/30" data-component="speed-dial-backdrop" onClick={() => toggleOpen('click on div backdrop')} />
+        <div className="absolute bottom-0 right-0 z-10 h-screen w-screen bg-black/30" data-component="speed-dial-backdrop" onClick={toggleOpen('click on div backdrop')} />
       </Fade>
       <Zoom in={isVisible}>
         <div className="fixed bottom-10 right-10 z-20 print:hidden" data-component="speed-dial">
           {/* @ts-expect-error typings issue */}
-          <SpeedDial ariaLabel='Actions' icon={<SpeedDialIcon />} onClick={() => toggleOpen('click on dial')} onMouseEnter={() => { if (!scout.isMobile) { logger.debug('open cause mouse enter'); setOpen(true) } }} onMouseLeave={() => { if (!scout.isMobile) { logger.debug('close cause mouse leave'); setOpen(false) } }} open={isOpen}>
+          <SpeedDial ariaLabel='Actions' icon={<SpeedDialIcon />} onClick={toggleOpen('click on dial')} onMouseEnter={onMouse('enter')} onMouseLeave={onMouse('leave')} open={isOpen}>
             {/* @ts-expect-error typings issue */}
             {actions.map((action) => /* @ts-expect-error typings issue */ (
               <SpeedDialAction icon={action.icon} key={action.name} onClick={action.handleClick} tooltipTitle={action.name} />
--- src/pages/page-sequencer.tsx
+++ src/pages/page-sequencer.tsx
@@ -12,7 +12,7 @@ import { useSignalEffect } from '@preact/signals'
 import { useEffect, useState } from 'preact/hooks'
 import { copyToClipboard, debounce } from 'shuutils'
 import { AppPageCard } from '../components/app-page-card'
-import { delays } from '../constants'
+import { delays, voidFunction } from '../constants'
 import { logger } from '../utils/logger.utils'
 import { playSequence, sequences, type Sequence, type SequenceItem, type SequenceItemTone, type SequenceItemWait } from '../utils/sound.utils'
 import { state } from '../utils/state.utils'
@@ -23,35 +23,38 @@ const ranges = {
   waitDelay: { max: 1000, min: 25 },
 }
 
+type ChangeCallback = (index: number, type: 'tone-delay' | 'tone-value' | 'wait-delay') => (event: Event, value: number[] | number) => void
+
 function copySequence (sequence: Sequence) {
   logger.debug('copySequence', { sequence })
   void copyToClipboard(JSON.stringify(sequence))
   state.message = { content: 'Sequence copied to clipboard', delay: delays.second, type: 'success' }
+  return voidFunction
 }
 
 const playSequenceDebounced = debounce(playSequence, delays.medium)
 
-function AppSequenceItemTone ({ index, item, onChange }: { readonly index: number; readonly item: SequenceItemTone; readonly onChange: (event: Event, value: number[] | number, index: number, type: 'tone-delay' | 'tone-value' | 'wait-delay') => void }) {
+function AppSequenceItemTone ({ index, item, onChange }: { readonly index: number; readonly item: SequenceItemTone; readonly onChange: ChangeCallback }) {
   return <>
     <div className="flex w-2/3">
-      <Slider max={ranges.toneValue.max} min={ranges.toneValue.min} onChange={(event, value) => onChange(event, value, index, 'tone-value')} step={ranges.toneValue.min} value={item[0]} />
+      <Slider max={ranges.toneValue.max} min={ranges.toneValue.min} onChange={onChange(index, 'tone-value')} step={ranges.toneValue.min} value={item[0]} />
     </div>
     <div className="flex w-1/3">
-      <Slider max={ranges.toneDelay.max} min={ranges.toneDelay.min} onChange={(event, value) => onChange(event, value, index, 'tone-delay')} step={ranges.toneDelay.min} value={item[1]} />
+      <Slider max={ranges.toneDelay.max} min={ranges.toneDelay.min} onChange={onChange(index, 'tone-delay')} step={ranges.toneDelay.min} value={item[1]} />
     </div>
   </>
 }
 
-function AppSequenceItemWait ({ index, item, onChange }: { readonly index: number; readonly item: SequenceItemWait; readonly onChange: (event: Event, value: number[] | number, index: number, type: 'tone-delay' | 'tone-value' | 'wait-delay') => void }) {
+function AppSequenceItemWait ({ index, item, onChange }: { readonly index: number; readonly item: SequenceItemWait; readonly onChange: ChangeCallback }) {
   return <>
     <div className="flex w-2/3" />
     <div className="flex w-1/3">
-      <Slider max={ranges.waitDelay.max} min={ranges.waitDelay.min} onChange={(event, value) => onChange(event, value, index, 'wait-delay')} step={ranges.waitDelay.min} value={item} />
+      <Slider max={ranges.waitDelay.max} min={ranges.waitDelay.min} onChange={onChange(index, 'wait-delay')} step={ranges.waitDelay.min} value={item} />
     </div>
   </>
 }
 
-function AppSequenceItem ({ index, item, onChange, onDelete }: { readonly index: number; readonly item: SequenceItem; readonly onChange: (event: Event, value: number[] | number, index: number, type: 'tone-delay' | 'tone-value' | 'wait-delay') => void; readonly onDelete: (index: number) => void }) {
+function AppSequenceItem ({ index, item, onChange, onDelete }: { readonly index: number; readonly item: SequenceItem; readonly onChange: ChangeCallback; readonly onDelete: (index: number) => void }) {
   const id = `${typeof item === 'number' ? 'W' : 'T'}${index}`
   const label = typeof item === 'number' ? `Wait for ${item}ms` : `Tone ${item[0]}Hz for ${item[1]}ms`
   const isEven = index % 2 === 0 // eslint-disable-line @typescript-eslint/no-magic-numbers
@@ -61,7 +64,7 @@ function AppSequenceItem ({ index, item, onChange, onDelete }: { readonly index:
       <p className="w-1/3">{label}</p>
       {typeof item === 'number' && <AppSequenceItemWait index={index} item={item} onChange={onChange} />}
       {typeof item !== 'number' && <AppSequenceItemTone index={index} item={item} onChange={onChange} />}{/* @ts-expect-error typings issue */}
-      <IconButton aria-label="delete" onClick={() => onDelete(index)}><DeleteIcon /></IconButton>
+      <IconButton aria-label="delete" onClick={onDelete(index)}><DeleteIcon /></IconButton>
       <p className="font-mono">{id}</p>
     </div>
   )
@@ -71,11 +74,19 @@ export function PageSequencer ({ ...properties }: { readonly [key: string]: unkn
 
   const [sequence, setSequence] = useState<Sequence>(sequences.success)
 
-  function onChange (_event: Event, value: number[] | number, index: number, type: 'tone-delay' | 'tone-value' | 'wait-delay') { // eslint-disable-line no-underscore-dangle, @typescript-eslint/naming-convention, @typescript-eslint/max-params, sonarjs/cognitive-complexity
-    if (typeof value !== 'number') throw new Error('Weird case where value is not a number')
-    if (type === 'wait-delay') setSequence(sequence.map((item, itemIndex) => (itemIndex === index ? value : item))) /* @ts-expect-error typings issue */
-    else if (type === 'tone-delay') setSequence(sequence.map((item, itemIndex) => (itemIndex === index ? [item[0], value] : item))) /* @ts-expect-error typings issue */
-    else setSequence(sequence.map((item, itemIndex) => (itemIndex === index ? [value, item[1]] : item)))
+  function updateSequence (updatedSequence: Sequence) {
+    logger.debug('updateSequence', { updatedSequence })
+    setSequence(updatedSequence)
+    return voidFunction
+  }
+
+  function onChange (index: number, type: 'tone-delay' | 'tone-value' | 'wait-delay') { // eslint-disable-line sonarjs/cognitive-complexity
+    return (_event: Event, value: number[] | number) => { // eslint-disable-line no-underscore-dangle, @typescript-eslint/naming-convention
+      if (typeof value !== 'number') throw new Error('Weird case where value is not a number')
+      if (type === 'wait-delay') setSequence(sequence.map((item, itemIndex) => (itemIndex === index ? value : item))) /* @ts-expect-error typings issue */
+      else if (type === 'tone-delay') setSequence(sequence.map((item, itemIndex) => (itemIndex === index ? [item[0], value] : item))) /* @ts-expect-error typings issue */
+      else setSequence(sequence.map((item, itemIndex) => (itemIndex === index ? [value, item[1]] : item)))
+    }
   }
 
   function onDelete (index: number) {
@@ -93,13 +104,13 @@ export function PageSequencer ({ ...properties }: { readonly [key: string]: unkn
           {sequence.map((item, index) => <AppSequenceItem index={index} item={item} key={`${typeof item === 'number' ? 'wait' : 'tone'}-${index}`} onChange={onChange} onDelete={onDelete} />)}
         </form>
         <div className="flex w-full">{/* @ts-expect-error typings issue */}
-          <Button onClick={() => setSequence(sequences.success)} startIcon={<RestartAltIcon />} variant="outlined">Reset</Button>
+          <Button onClick={updateSequence(sequences.success)} startIcon={<RestartAltIcon />} variant="outlined">Reset</Button>
           <div className="grow" />
-          <Button onClick={() => setSequence([...sequence, delays.small])} variant="outlined">Add wait</Button>{/* @ts-expect-error typings issue */ /* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+          <Button onClick={updateSequence([...sequence, delays.small])} variant="outlined">Add wait</Button>{/* @ts-expect-error typings issue */ /* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
           <Button onClick={async () => await playSequence(sequence)} startIcon={<PlayCircleFilledWhiteOutlinedIcon />} variant="contained">Play</Button>{/* eslint-disable-next-line @typescript-eslint/no-magic-numbers */}
-          <Button onClick={() => setSequence([...sequence, [delays.small, delays.large * 2]])} variant="outlined">Add tone</Button>
+          <Button onClick={updateSequence([...sequence, [delays.small, delays.large * 2]])} variant="outlined">Add tone</Button>
           <div className="grow" />{/* @ts-expect-error typings issue */}
-          <Button endIcon={<ContentCopyIcon />} onClick={() => copySequence(sequence)} variant="outlined">Copy</Button>
+          <Button endIcon={<ContentCopyIcon />} onClick={copySequence(sequence)} variant="outlined">Copy</Button>
         </div>
       </div>
     </AppPageCard>
