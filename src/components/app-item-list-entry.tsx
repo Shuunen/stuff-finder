b39d45c50a1a353a6630e05e1265b4d39ac8dc34
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-01-17 22:29:31 +0100

    enhance item list

--- src/components/app-item-list-entry.tsx
+++ src/components/app-item-list-entry.tsx
@@ -5,13 +5,17 @@ import type { Item } from '../types/item.types'
 import { itemToImageUrl, onItemImageError } from '../utils/item.utils'
 
 export function AppItemListEntry ({ item }: { readonly item: Item }) {
+
+  const title = `${item.name}${item.brand ? ` - ${item.brand.trim()}` : ''}` // eslint-disable-line sonarjs/no-nested-template-literals
+  const subtitle = `${item.category} - ${item.box.split(' ')[0] ?? ''}${item.drawer}`
+
   return (
     <ListItem disablePadding key={item.id}>
       {/* @ts-expect-error typings issue */}
       <ListItemButton component="a" href={`/item/details/${item.id}`}>
         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-        <img alt={item.name} className="mr-4 size-12 rounded-full" data-id={item.id} onError={onItemImageError} src={itemToImageUrl(item)} />
-        <ListItemText primary={item.name} />
+        <img alt={title} className="mr-4 size-12 rounded-full object-contain" data-id={item.id} onError={onItemImageError} src={itemToImageUrl(item)} />
+        <ListItemText primary={title} primaryTypographyProps={{ fontSize: 18 }} secondary={subtitle} secondaryTypographyProps={{ fontSize: 16 }} />
       </ListItemButton>
     </ListItem>
   )
