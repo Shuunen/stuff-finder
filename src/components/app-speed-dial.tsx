260b20dc15c4378ad3cbfab510e3c4da001d55b6
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-02-17 12:23:47 +0100

    dial visible by default

--- src/components/app-speed-dial.tsx
+++ src/components/app-speed-dial.tsx
@@ -8,12 +8,10 @@ import SettingsIcon from '@mui/icons-material/Settings'
 import Fade from '@mui/material/Fade'
 import SpeedDial from '@mui/material/SpeedDial'
 import SpeedDialAction from '@mui/material/SpeedDialAction'
-import Zoom from '@mui/material/Zoom'
 import { route } from 'preact-router'
 import { useState } from 'preact/hooks'
 import { scout } from '../utils/browser.utils'
 import { logger } from '../utils/logger.utils'
-import { state, watchState } from '../utils/state.utils'
 
 const actions = [
   { handleClick: () => route('/'), icon: <HomeIcon />, name: 'Home' },
@@ -25,26 +23,22 @@ const actions = [
 
 export function AppSpeedDial ({ isLoading }: { readonly isLoading: boolean }) {
 
-  const [isVisible, setIsVisible] = useState(false)
   const [isOpen, setOpen] = useState(false)
   function toggleOpen (reason = 'unknown') { logger.debug('toggle cause', reason); setOpen(!isOpen) }
   function onMouse (status: 'enter' | 'leave') { if (!scout.isMobile) { logger.debug('open cause mouse', status); setOpen(status === 'enter') } }
-  watchState('status', () => setIsVisible(state.status === 'ready'))
 
   return (
     <>
       <Fade in={isOpen}>
         <div className="absolute bottom-0 right-0 z-10 size-full bg-black/30" data-component="speed-dial-backdrop" onClick={() => toggleOpen('click on div backdrop')} />
       </Fade>
-      <Zoom in={isVisible}>
-        <div className="fixed bottom-10 right-10 z-20 print:hidden" data-component="speed-dial">
-          <SpeedDial ariaLabel='Actions' FabProps={{ color: isLoading ? 'warning' : 'primary' }} icon={isLoading ? <HourglassTop /> : <SpeedDialIcon />} onClick={() => toggleOpen('click on dial')} onMouseEnter={() => onMouse('enter')} onMouseLeave={() => onMouse('leave')} open={isOpen}>
-            {actions.map((action) => (
-              <SpeedDialAction icon={action.icon} key={action.name} onClick={action.handleClick} tooltipTitle={action.name} />
-            ))}
-          </SpeedDial>
-        </div>
-      </Zoom>
+      <div className="fixed bottom-10 right-10 z-20 print:hidden" data-component="speed-dial">
+        <SpeedDial ariaLabel='Actions' FabProps={{ color: isLoading ? 'warning' : 'primary' }} icon={isLoading ? <HourglassTop /> : <SpeedDialIcon />} onClick={() => toggleOpen('click on dial')} onMouseEnter={() => onMouse('enter')} onMouseLeave={() => onMouse('leave')} open={isOpen}>
+          {actions.map((action) => (
+            <SpeedDialAction icon={action.icon} key={action.name} onClick={action.handleClick} tooltipTitle={action.name} />
+          ))}
+        </SpeedDial>
+      </div>
     </>
   )
 }
