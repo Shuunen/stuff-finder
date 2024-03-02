81f438051645962b86b648e981fff308d5e4289e
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-03-02 15:00:33 +0100

    regroup & test suggestions

--- src/utils/suggestions.utils.ts
+++ src/utils/suggestions.utils.ts
@@ -1,6 +1,74 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { capitalize, clone } from 'shuutils'
+import { emptyItemSuggestions } from '../constants'
+import type { WrapApiAliExResponse, WrapApiAngboResponse, WrapApiCampoResponse, WrapApiDeyesResponse } from '../types/requests.types'
+import { get } from './browser.utils'
+import { logger } from './logger.utils'
+import type { ItemSuggestions } from './parsers.utils'
+import { state } from './state.utils'
+import { getAsin } from './url.utils'
 
 const keysToCapitalize = new Set(['name', 'details'])
+
+function priceParse (price?: number | string) {
+  if (price === undefined) return ''
+  if (typeof price === 'string') return Math.round(Number.parseFloat(price)).toString()
+  return Math.round(price).toString()
+}
+
+export async function addSuggestionsFromWrap<ResponseType> (endpoint: string, getMethod = get) {
+  const wrapApiKey = state.credentials.wrap
+  if (wrapApiKey === '') return {} as ResponseType // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  return await getMethod(`https://wrapapi.com/use/jojo/${endpoint}&wrapAPIKey=${wrapApiKey}`) as ResponseType // eslint-disable-line @typescript-eslint/consistent-type-assertions
+}
+
+export async function addSuggestionsFromDeyes (suggestions: ItemSuggestions, code: string, getMethod = get) {
+  const { data, success } = await addSuggestionsFromWrap<WrapApiDeyesResponse>(`deyes/json/0.0.2?code=${code}`, getMethod)
+  if (!success) return
+  logger.debug('deyes data', data)
+  suggestions.name.push(data.name)
+  suggestions.brand.push(data.brand.name)
+  suggestions.details.push(data.description)
+  const [image] = data.image
+  if (image !== undefined) suggestions.photo.push(image)
+  suggestions.price.push(priceParse(data.offers.price))
+  suggestions.reference.push(data.gtin13)
+}
+
+export async function addSuggestionsFromAngbo (suggestions: ItemSuggestions, str: string, getMethod = get) {
+  const { data, success } = await addSuggestionsFromWrap<WrapApiAngboResponse>(`angbo/search/0.0.3?id=${str}`, getMethod)
+  if (!success) return
+  logger.debug('angbo data', data)
+  suggestions.name.push(data.title)
+  suggestions.photo.push(data.photo)
+  suggestions.price.push(priceParse(data.price))
+  suggestions.reference.push(data.asin)
+}
+
+export async function addSuggestionsFromAliEx (suggestions: ItemSuggestions, str: string, getMethod = get) {
+  const { data, success } = await addSuggestionsFromWrap<WrapApiAliExResponse>(`aliex/search/0.0.1?str=${str}`, getMethod)
+  if (!success) return
+  logger.debug('AliEx data', data)
+  data.items.forEach(item => {
+    suggestions.name.push(item.title)
+    suggestions.photo.push(item.photo)
+    suggestions.price.push(priceParse(item.price))
+    suggestions.reference.push(item.reference)
+  })
+}
+
+export async function addSuggestionsFromCampo (suggestions: ItemSuggestions, str: string, getMethod = get) {
+  const { data, success } = await addSuggestionsFromWrap<WrapApiCampoResponse>(`alcampo/search/0.0.3?str=${str}`, getMethod)
+  if (!success) return
+  logger.debug('campo data', data)
+  data.items.forEach(item => {
+    suggestions.brand.push(item.brand)
+    suggestions.name.push(item.title)
+    suggestions.photo.push(item.photo)
+    if (item.price !== undefined) suggestions.price.push(priceParse(item.price))
+    suggestions.reference.push(item.uuid)
+  })
+}
 
 export function cleanSuggestions (suggestionsInput: Record<string, string[] | undefined>) {
   const suggestions = clone(suggestionsInput)
@@ -14,4 +82,14 @@ export function cleanSuggestions (suggestionsInput: Record<string, string[] | un
   })
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
   return suggestions as Record<string, string[]>
+}
+
+export async function getSuggestions (str: string) {
+  const asin = getAsin(str)
+  const suggestionsBase = clone(emptyItemSuggestions)
+  if (asin !== undefined) await addSuggestionsFromAngbo(suggestionsBase, asin)
+  if (suggestionsBase.name.length === 0) await addSuggestionsFromDeyes(suggestionsBase, str)
+  if (suggestionsBase.name.length === 0) await addSuggestionsFromAliEx(suggestionsBase, str)
+  await addSuggestionsFromCampo(suggestionsBase, str)
+  return cleanSuggestions(suggestionsBase)
 }
--- tests/suggestions.test.ts
+++ tests/suggestions.test.ts
@@ -1,5 +1,12 @@
-import { expect, it } from 'vitest'  
-import { cleanSuggestions } from '../src/utils/suggestions.utils'
+/* eslint-disable @typescript-eslint/naming-convention */
+import { clone } from 'shuutils'
+import { expect, it, vi } from 'vitest'
+import { emptyItemSuggestions } from '../src/constants'
+import { get } from '../src/utils/browser.utils'
+import { mockItem } from '../src/utils/mock.utils'
+import { state } from '../src/utils/state.utils'
+import { addSuggestionsFromAliEx, addSuggestionsFromAngbo, addSuggestionsFromCampo, addSuggestionsFromDeyes, addSuggestionsFromWrap, cleanSuggestions, getSuggestions } from '../src/utils/suggestions.utils'
+
 
 it('cleanSuggestions A', () => { expect(cleanSuggestions({ keyA: ['a', 'b', 'c'], keyB: ['a', 'b', 'c'] })).toEqual({ keyA: ['a', 'b', 'c'], keyB: ['a', 'b', 'c'] }) })
 it('cleanSuggestions B', () => { expect(cleanSuggestions({})).toEqual({}) })
@@ -7,3 +14,82 @@ it('cleanSuggestions C', () => { expect(cleanSuggestions({ keyA: ['a', 'b', 'c']
 it('cleanSuggestions D', () => { expect(cleanSuggestions({ keyA: ['a', 'b', 'c'], keyB: ['a', 'b', 'c', 'a', 'b', 'c'] })).toEqual({ keyA: ['a', 'b', 'c'], keyB: ['a', 'b', 'c'] }) })
 it('cleanSuggestions E', () => { expect(cleanSuggestions({ name: ['a', 'b', 'c'] })).toEqual({ name: ['A', 'B', 'C'] }) })
 it('cleanSuggestions F', () => { expect(cleanSuggestions({ details: ['a', 'b', 'c'], plop: undefined })).toEqual({ details: ['A', 'B', 'C'] }) })
+
+it('getSuggestions A', async () => {
+  const url = 'https://www.amazon.fr/dp/B07V7GZQJ2'
+  const suggestions = await getSuggestions(url)
+  expect(suggestions).toMatchInlineSnapshot(`
+    {
+      "status": [
+        "acheté",
+      ],
+    }
+  `)
+})
+
+it('addSuggestionsFromWrap A without wrapApiKey', async () => {
+  state.credentials.wrap = ''
+  const endpoint = 'deyes/json/0.0.2?code=3760052142741'
+  const suggestions = await addSuggestionsFromWrap(endpoint)
+  expect(suggestions).toMatchInlineSnapshot('{}')
+})
+
+it('addSuggestionsFromWrap B with wrapApiKey', async () => {
+  state.credentials.wrap = 'xyz'
+  const endpoint = 'deyes/json/0.0.2?code=3760052142741'
+  const suggestions = await addSuggestionsFromWrap(endpoint)
+  expect(suggestions).toMatchSnapshot()
+})
+
+it('addSuggestionsFromCampo A without wrapApiKey', async () => {
+  state.credentials.wrap = ''
+  const suggestions = clone(emptyItemSuggestions)
+  await addSuggestionsFromCampo(suggestions, '3760052142741')
+  expect(suggestions).toStrictEqual(emptyItemSuggestions)
+})
+
+it('addSuggestionsFromCampo B with working get', async () => {
+  state.credentials.wrap = 'xyz'
+  const suggestions = clone(emptyItemSuggestions)
+  const mock = vi.fn().mockImplementation(get)
+  mock.mockImplementationOnce(() => ({ data: { items: [mockItem({ price: 42 })] }, success: true }))
+  await addSuggestionsFromCampo(suggestions, '3760052142741', mock)
+  expect(mock).toHaveBeenCalledTimes(1)
+  expect(mock).toHaveBeenCalledWith('https://wrapapi.com/use/jojo/alcampo/search/0.0.3?str=3760052142741&wrapAPIKey=xyz')
+  expect(suggestions).toMatchSnapshot()
+})
+
+it('addSuggestionsFromAliEx A with working get', async () => {
+  state.credentials.wrap = 'xyz'
+  const suggestions = clone(emptyItemSuggestions)
+  const title = 'title A'
+  const mock = vi.fn().mockImplementation(get)
+  mock.mockImplementationOnce(() => ({ data: { items: [{ title }] }, success: true }))
+  await addSuggestionsFromAliEx(suggestions, '3760052142741', mock)
+  expect(mock).toHaveBeenCalledTimes(1)
+  expect(mock).toHaveBeenCalledWith('https://wrapapi.com/use/jojo/aliex/search/0.0.1?str=3760052142741&wrapAPIKey=xyz')
+  expect(suggestions).toMatchSnapshot()
+})
+
+it('addSuggestionsFromAngbo A with working get', async () => {
+  state.credentials.wrap = 'xyz'
+  const suggestions = clone(emptyItemSuggestions)
+  const title = 'title A'
+  const mock = vi.fn().mockImplementation(get)
+  mock.mockImplementationOnce(() => ({ data: { title }, success: true }))
+  await addSuggestionsFromAngbo(suggestions, '3760052142741', mock)
+  expect(mock).toHaveBeenCalledTimes(1)
+  expect(mock).toHaveBeenCalledWith('https://wrapapi.com/use/jojo/angbo/search/0.0.3?id=3760052142741&wrapAPIKey=xyz')
+  expect(suggestions).toMatchSnapshot()
+})
+
+it('addSuggestionsFromDeyes A with working get', async () => {
+  state.credentials.wrap = 'xyz'
+  const suggestions = clone(emptyItemSuggestions)
+  const mock = vi.fn().mockImplementation(get)
+  mock.mockImplementationOnce(() => ({ data: { brand: { name: 'brand A' }, description: '', gtin13: 'gtin13azdaz4878', image: [{ image: undefined }], name: 'name A', offers: { price: '42.50' } }, success: true }))
+  await addSuggestionsFromDeyes(suggestions, '3760052142741', mock)
+  expect(mock).toHaveBeenCalledTimes(1)
+  expect(mock).toHaveBeenCalledWith('https://wrapapi.com/use/jojo/deyes/json/0.0.2?code=3760052142741&wrapAPIKey=xyz')
+  expect(suggestions).toMatchSnapshot()
+})
