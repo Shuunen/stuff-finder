978a5ff2d29e9d06a8156b0ac20a81efd663b440
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-05-29 17:31:17 +0200

    increase leading on barcode label

--- src/components/app-barcode.tsx
+++ src/components/app-barcode.tsx
@@ -17,7 +17,7 @@ export function AppBarcode ({ isHighlighted = false, item, size }: Readonly<{ is
         <qr-code data={value} margin={0} modulesize={3} />
       </div>
       <div className="overflow-hidden pl-1.5 pt-1 text-center">
-        <div className={`mb-1 line-clamp-3 font-sans text-[12px] leading-3 tracking-[-0.5px] ${isHighlighted ? 'bg-red-400' : ''}`} >{printText}</div>
+        <div className={`mb-1 line-clamp-3 font-sans text-[12px] leading-4 tracking-[-0.5px] ${isHighlighted ? 'bg-red-400' : ''}`} >{printText}</div>
         <div className={`mb-1 font-mono text-[19px] font-bold leading-none tracking-[2px] ${isHighlighted ? 'bg-blue-400' : ''}`}>{printLocation}</div>
       </div>
     </div>
