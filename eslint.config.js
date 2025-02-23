c8edde1fcc6661fcf0c8dbd514946ef77fba919b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-02-23 15:37:17 +0100

    feat(eslint): avoid throw statements to use result pattern instead

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import shuunen from 'eslint-plugin-shuunen'
 
+const noRestrictedSyntaxRule = shuunen.configs.base[1]?.rules?.['no-restricted-syntax']
+const /** @type {object[]} */ noRestrictedSyntaxRules = Array.isArray(noRestrictedSyntaxRule) ? noRestrictedSyntaxRule.slice(1) : []
+
 // biome-ignore lint/style/noDefaultExport: <explanation>
 export default [
   ...shuunen.configs.base,
@@ -11,6 +14,15 @@ export default [
     name: "stuff-finder-overrides",
     rules: {
       '@typescript-eslint/prefer-readonly-parameter-types': 'off', // annoying
+      'no-restricted-syntax': [
+        'error',
+        ...noRestrictedSyntaxRules,
+        {
+          message: 'Throw statements are not allowed, use Result pattern or something else',
+          selector: 'ThrowStatement',
+        },
+      ],
+      'unicorn/no-useless-undefined': 'off', // annoying
     },
   },
   {
