40f854d084cdac6b451fe93dd08925e6ddfe1305
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-02-23 15:39:26 +0100

    feat(strings): add sadAscii function and corresponding test

--- src/utils/strings.test.ts
+++ src/utils/strings.test.ts
@@ -1,6 +1,10 @@
 import { expect, it } from 'vitest'
-import { coolAscii } from './strings.utils'
+import { coolAscii, sadAscii } from './strings.utils'
 
 it('coolAscii A', () => {
   expect(coolAscii().length > 0).toBe(true)
+})
+
+it('sadAscii A', () => {
+  expect(sadAscii().length > 0).toBe(true)
 })
--- src/utils/strings.utils.ts
+++ src/utils/strings.utils.ts
@@ -2,8 +2,16 @@ import { pickOne } from 'shuutils'
 
 /**
  * Returns a random cool ascii face
- * @returns {string} A random cool ascii face
+ * @returns a random cool ascii face
  */
 export function coolAscii () {
   return pickOne(['( ＾◡＾)', '♥‿♥', '八(＾□＾*)', '(◡ ‿ ◡ ✿)', '(=^ェ^=)', 'ʕ •ᴥ•ʔ', '(*°∀°)', String.raw`\(-ㅂ-)/`, 'ლ(╹◡╹ლ)', 'ლ(o◡oლ)', '＼(＾O＾)／'])
+}
+
+/**
+ * Returns a random sad ascii face
+ * @returns a random sad ascii face
+ */
+export function sadAscii () {
+  return pickOne(['(╥﹏╥)', '(T_T)', '(×_×)', '(TдT)', '(╯︵╰,)'])
 }
