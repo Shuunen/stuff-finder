b3f48f7f21a6ee20e9910d856ce0f1fba6bf70e9
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-02-23 15:38:36 +0100

    refactor(app): migrate legacy code

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
 import { render } from 'preact'
+import { toastError, toastInfo } from 'shuutils'
 import { App } from './app.tsx'
 import './assets/styles.css'
-import './main.to.migrate.ts'
+import { getItems } from './utils/item.utils.ts'
+import { logger } from './utils/logger.utils.ts'
+import { state } from './utils/state.utils.ts'
+
+logger.info('app start')
 
 const root = document.querySelector('#app')
-if (!root) throw new Error('No root element found!')
-render(<App />, root)
+if (root) render(<App />, root)
+else logger.error('root not found')
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+void getItems().then(result => {
+  state.status = result.ok ? 'ready' : 'settings-required'
+  if (result.ok) toastInfo(result.value)
+  else if (typeof result.error === 'string') toastError(result.error)
+})
