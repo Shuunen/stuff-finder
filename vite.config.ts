73738eab8e63af084f1a3ce4da094c0c9d4e399b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-05-27 21:19:10 +0200

    fix: remove reportsDirectory from Vitest coverage configuration

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,6 @@ export default defineConfig({
       include: ['src'],
       provider: 'v8' as const,
       reporter: [['text', { maxCols: 120 }], 'lcov'],
-      reportsDirectory: './test-output/vitest/coverage',
       thresholds: {
         100: true,
       },
