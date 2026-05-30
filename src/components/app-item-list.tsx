568f51988c6e61922ca481df6e006d1f15cd8482
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-05-30 15:12:15 +0200

    refactor: update AppItemList component for improved layout and styling

--- src/components/app-item-list.tsx
+++ src/components/app-item-list.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useMemo, useRef, useState } from 'react'
+import { cn } from 'shuutils'
 import type { Item } from '../types/item.types'
 import type { Display } from '../types/theme.types'
 import { state, watchState } from '../utils/state.utils'
@@ -13,7 +14,7 @@ type Props = Readonly<{
 }>
 
 export function AppItemList(props: Props) {
-  const { display: displayProp, items, loadingItemIds, onSelection, showPrice } = props
+  const { display: displayProp, items, loadingItemIds, onSelection } = props
   const loadingSet = useMemo(() => new Set(loadingItemIds), [loadingItemIds])
   const [display, setDisplay] = useState<Display>(displayProp ?? state.display)
   // handle selection
@@ -36,10 +37,10 @@ export function AppItemList(props: Props) {
       setDisplay(state.display)
     })
   return (
-    <nav aria-label="item list" className="mb-20 overflow-x-hidden overflow-y-auto md:mb-0" data-component="item-list">
-      <div className={`grid grid-cols-1 bg-gray-100 ${display === 'list' ? '' : 'gap-3 p-3 xs:grid-cols-2 sm:grid-cols-3 sm:gap-5 sm:p-5'}`} data-type="list">
+    <nav aria-label="item list" className="mb-24 overflow-x-hidden overflow-y-auto" data-component="item-list">
+      <div className={cn('grid p-4', display === 'card' ? 'grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4' : 'gap-4')} data-type="list">
         {items.map(item => (
-          <AppItemListEntry display={display} isLoading={loadingSet.has(item.$id)} item={item} key={item.$id} onSelect={onSelection ? onSelect : undefined} showPrice={showPrice} />
+          <AppItemListEntry display={display} isLoading={loadingSet.has(item.$id)} item={item} key={item.$id} onSelect={onSelection ? onSelect : undefined} />
         ))}
       </div>
     </nav>
